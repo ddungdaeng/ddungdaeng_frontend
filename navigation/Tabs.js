ee68e147ff291ff265cbb6f547a39c3d6d6c0f44
yxxndxxn <yxxndxxn@gmail.com>
2025-05-20 14:46:50 +0900

    ✨ feat: tab, stack navigation 세팅

---
+++ navigation/Tabs.js
@@ -0,0 +1,44 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import TodayWeight from "../screens/TodayWeight";
+import TodayFood from "../screens/TodayFood";
+import TodayWalk from "../screens/TodayWalk";
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="TodayWeight"
+        component={TodayWeight}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return <Ionicons name="film" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="TodayFood"
+        component={TodayFood}
+        options={{
+          tabBarBadge: 4,
+          tabBarIcon: ({ focused, color, size }) => {
+            return <Ionicons name="tv" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="TodayWalk"
+        component={TodayWalk}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return <Ionicons name="search" color={color} size={size} />;
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Tabs;
---
+++ screens/TodayWeight.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function TodayWeight() {
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>TodayWeight</Text>
+  </View>;
+}
