f09d236cf40e8c5e66dfdf59f349872849fc920d
yxxndxxn <yxxndxxn@gmail.com>
2025-07-01 20:53:51 +0900

    🔧 fix: 작업 환경 수정

--- screens/tabs/TodayFood.tsx
+++ screens/tabs/TodayFood.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 export default function TodayFood() {
   return (
-    <View className="p-5 bg-red-500 rounded-lg">
+    <View>
       <Text>TodayFood</Text>
     </View>
   );
