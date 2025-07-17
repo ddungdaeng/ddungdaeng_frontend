a6b8da1d5708143156a8ca8efb9f521589ef8f17
yxxndxxn <yxxndxxn@gmail.com>
2025-07-18 01:35:02 +0900

    ⚡️ feat: 바 차트 구현

--- src/components/change/LineChartCard.tsx
+++ src/components/change/LineChartCard.tsx
@@ -8,12 +8,16 @@ type LineChartCardProps = {
   style?: ViewStyle;
   data?: { value: number; date?: string; label?: string }[];
   title: string;
+  maxValue: number;
+  startValue: number;
 };
 
 export default function LineChartCard({
   style,
   data,
   title,
+  maxValue,
+  startValue,
 }: LineChartCardProps) {
   const { width: screenWidth } = Dimensions.get("window");
   const chartWidth = screenWidth - 48 - 90; // 총 여백을 한 번에 계산
@@ -24,12 +28,13 @@ export default function LineChartCard({
       <View style={[styles.card, style]}>
         {data ? (
           <LineChart
-            isAnimated
+            // isAnimated
             animationDuration={1000}
             data={data}
+            maxValue={maxValue} // 최대값 설정(데이터 시작 지점에서 출발임: yAxisOffset)
+            yAxisOffset={startValue} //데이터 시작 지점
             showValuesAsDataPointsText //점 위에 데이터 텍스트 표시
             textColor1={colors.gray2}
-            yAxisOffset={6} //데이터 시작 지점
             thickness={2}
             dataPointsColor1={colors.gray2} //점 색상
             color={colors.gray2} //라인 색상
@@ -42,10 +47,10 @@ export default function LineChartCard({
             xAxisColor={colors.gray4}
             yAxisColor={colors.gray4}
             yAxisLabelSuffix="kg" //단위
-            yAxisTextStyle={styles.yAxisTextStyle}
-            xAxisLabelTextStyle={styles.xAxisLabelTextStyle}
+            yAxisTextStyle={styles.textStyle}
+            xAxisLabelTextStyle={styles.textStyle}
             stepValue={0.3} // Y축 간격
-            maxValue={1} // 최대값 설정(데이터 시작 지점에서 출발임: yAxisOffset)
+            yAxisThickness={0}
             // 차트 크기 제어
             width={chartWidth}
             height={100}
@@ -80,12 +85,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  yAxisTextStyle: {
-    color: colors.gray2,
-    fontSize: 10,
-    fontFamily: "Pretendard-Regular",
-  },
-  xAxisLabelTextStyle: {
+  textStyle: {
     color: colors.gray2,
     fontSize: 10,
     fontFamily: "Pretendard-Regular",
--- src/components/dashboard/WeightChart.tsx
+++ src/components/dashboard/WeightChart.tsx
@@ -56,6 +56,38 @@ export default function WeightChart({ style, data }: WeightChartProps) {
             </CustomText>
           </View>
         )}
+        {/*todo: 여기에 차트 라이브러리 추가 */}
+        <LineChart
+          isAnimated
+          animationDuration={1000}
+          data={data}
+          yAxisOffset={6} //데이터 시작 지점
+          thickness={2}
+          areaChart
+          color={colors.primary1}
+          startFillColor={"rgb(84,219,234)"}
+          endFillColor={"rgb(84,219,234)"}
+          startOpacity={0.4}
+          endOpacity={0.1}
+          xAxisColor={colors.gray4}
+          yAxisColor={colors.gray4}
+          yAxisLabelSuffix="kg" //단위
+          yAxisTextStyle={styles.yAxisTextStyle}
+          xAxisLabelTextStyle={styles.xAxisLabelTextStyle}
+          showDataPointOnFocus //점 노출 디폴트:false
+          stepValue={0.3} // Y축 간격
+          maxValue={1} // 최대값 설정(데이터 시작 지점에서 출발임: yAxisOffset)
+          yAxisThickness={0} //y축 선 없애기
+          //포커스
+          focusEnabled
+          focusedDataPointColor={colors.primary1}
+          focusedDataPointRadius={3}
+          focusTogether
+          showTextOnFocus
+          // 차트 크기 제어
+          width={250} // 차트 너비 명시적 설정
+          height={100}
+        />
       </View>
       <CustomText style={styles.userText}>최근 원재가 기록함</CustomText>
     </View>
