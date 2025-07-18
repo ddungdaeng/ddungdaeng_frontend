cb26da172d59b949d3579e9c7a0420a3b6a3796d
yxxndxxn <yxxndxxn@gmail.com>
2025-07-19 02:03:29 +0900

    🔧 fix: merge 오류 수정

--- src/components/dashboard/WeightChart.tsx
+++ src/components/dashboard/WeightChart.tsx
@@ -13,50 +13,6 @@ export default function WeightChart({ style, data }: WeightChartProps) {
   return (
     <View style={styles.container}>
       <View style={[styles.card, style]}>
-        {data ? (
-          <LineChart
-            isAnimated
-            animationDuration={1000}
-            data={data}
-            yAxisOffset={6} //데이터 시작 지점
-            thickness={2}
-            areaChart
-            color={colors.primary1}
-            startFillColor={"rgb(84,219,234)"}
-            endFillColor={"rgb(84,219,234)"}
-            startOpacity={0.4}
-            endOpacity={0.1}
-            xAxisColor={colors.gray4}
-            yAxisColor={colors.gray4}
-            yAxisLabelSuffix="kg" //단위
-            yAxisTextStyle={styles.yAxisTextStyle}
-            xAxisLabelTextStyle={styles.xAxisLabelTextStyle}
-            showDataPointOnFocus //점 노출 디폴트:false
-            stepValue={0.3} // Y축 간격
-            maxValue={1} // 최대값 설정(데이터 시작 지점에서 출발임: yAxisOffset)
-            //포커스
-            focusEnabled
-            focusedDataPointColor={colors.primary1}
-            focusedDataPointRadius={3}
-            focusTogether
-            showTextOnFocus
-            // 차트 크기 제어
-            width={250} // 차트 너비 명시적 설정
-            height={100}
-          />
-        ) : (
-          <View style={styles.textInCard}>
-            <CustomText
-              style={{
-                fontSize: 14,
-                color: colors.gray2,
-              }}
-            >
-              아직 등록된 데이터가 없어요!
-            </CustomText>
-          </View>
-        )}
-        {/*todo: 여기에 차트 라이브러리 추가 */}
         <LineChart
           isAnimated
           animationDuration={1000}
