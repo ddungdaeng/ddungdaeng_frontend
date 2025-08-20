3651bc9058dd1ba8bba65b2dc0227ae58927381e
yxxndxxn <yxxndxxn@gmail.com>
2025-08-21 03:40:49 +0900

    🎨 style: shadow 수정

--- src/styles/shadow.js
+++ src/styles/shadow.js
@@ -1,23 +1,25 @@
+import colors from "./colors";
+
 const shadows = {
   basic: {
-    shadowColor: "#000000",
+    shadowColor: colors.black,
     shadowOffset: {
       width: 0,
       height: 4,
     },
-    shadowOpacity: 0.12,
-    shadowRadius: 8,
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
     elevation: 5, // Android용
   },
 
   light: {
-    shadowColor: "#000000",
+    shadowColor: colors.black,
     shadowOffset: {
       width: 0,
       height: 2,
     },
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
     elevation: 3,
   },
 };
